a3cca3fd12e12db907570cede0643ac96bde6235
Evan <evanhnly@gmail.com>
2022-01-17 17:37:39 -0600

    Updated database parameters to connect to RDS database.

--- server_build.sh
+++ server_build.sh
@@ -5,6 +5,8 @@ export PATH=${GRADLE_HOME}/bin:${PATH}
 
 export PATH=${JAVA_HOME}/lib:${PATH}
 
+export CLASSPATH=var/server/mysql-connector-java-8.0.27.jar:$CLASSPATH
+
 cd var/server
 gradle build
 
