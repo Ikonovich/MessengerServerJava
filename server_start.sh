a13c23012fc1c08612fe9546401e69a5afe195ca
Evan <evanhnly@gmail.com>
2022-01-16 18:57:54 -0600

    Slight change to server_start.sh

--- server_start.sh
+++ server_start.sh
@@ -1,5 +1,10 @@
 #This script initiates the server.
 
+wget https://services.gradle.org/distributions/gradle-5.0-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-5.0-bin.zip
+
+
+
 cd var/server
 gradle -v
 #java var/server/bin/messengerserver/Server
