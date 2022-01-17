c8da1738391030069833d4f2f4794566a0127c35
Evan <evanhnly@gmail.com>
2022-01-16 19:04:15 -0600

    Slight change to server_start.sh

--- server_start.sh
+++ server_start.sh
@@ -4,7 +4,7 @@ export GRADLE_HOME=/opt/gradle/gradle-5.0
 export PATH=${GRADLE_HOME}/bin:${PATH}
 
 cd var/server
-gradle -v
+gradle build
 #java var/server/bin/messengerserver/Server
 
 java bin/messengerserver/Server
