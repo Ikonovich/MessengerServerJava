dd44117bb4656f8c683ceaa5415823bfa6a3c74d
Evan <evanhnly@gmail.com>
2022-01-17 21:40:43 -0600

    Change to gradle build file

--- server_build.sh
+++ server_build.sh
@@ -6,6 +6,7 @@ export PATH=${GRADLE_HOME}/bin:${PATH}
 export PATH=${JAVA_HOME}/lib:${PATH}
 
 export CLASSPATH=$CLASSPATH:/var/server/mysql-connector-java-8.0.27.jar
+export CLASSPATH=$CLASSPATH:/var/server/gson-2.8.2.jar
 
 cd var/server
 gradle build
