e6a535ed927581247d044ef384b06cd556b85978
Evan <evanhnly@gmail.com>
2022-01-17 18:08:30 -0600

    Updated database parameters to connect to RDS database.

--- src/messengerserver/Server.java
+++ src/messengerserver/Server.java
@@ -6,6 +6,15 @@ public class Server {
 
 	public static void main(String[] args) 
 	{
+
+		try
+		{
+			Class.forName("org.mariadb.jdbc.Driver");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error");
+		}
 		System.out.println("Starting server.");
 
 		ServerController serverController = new ServerController();
